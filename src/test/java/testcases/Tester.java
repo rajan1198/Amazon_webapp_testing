ce468656deebac057291c45a48337750f154c9de
rajan1198 <rajanpawar110198@gmail.com>
2025-01-29 19:14:57 +0530

    code added again

--- src/test/java/testcases/Tester.java
+++ src/test/java/testcases/Tester.java
@@ -1,5 +1,15 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Tester {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("");
+	}
 
 }
